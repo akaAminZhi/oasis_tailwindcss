755033280c9f4b3f188224b6a94611cb5e02f67b
zhimin <min3832170@163.com>
2024-11-01 10:22:52 -0400

    start authiation

--- src/features/bookings/BookingRow.jsx
+++ src/features/bookings/BookingRow.jsx
@@ -5,9 +5,17 @@ import { formatCurrency } from "../../utils/helpers";
 import { formatDistanceFromNow } from "../../utils/helpers";
 import Tag from "../../ui/Tag";
 import Menus from "../../ui/Menus";
-import { HiArrowDownOnSquare, HiArrowUpOnSquare, HiEye } from "react-icons/hi2";
+import {
+  HiArrowDownOnSquare,
+  HiArrowUpOnSquare,
+  HiEye,
+  HiTrash,
+} from "react-icons/hi2";
 import { useNavigate } from "react-router-dom";
 import { useCheckout } from "../check-in-out/useCheckout";
+import Modal from "../../ui/Modal";
+import ComfirmDelete from "../../ui/ComfirmDelete";
+import { useDeleteBooking } from "./useDeleteBooking";
 function BookingRow({
   booking: {
     id: bookingId,
@@ -28,59 +36,75 @@ function BookingRow({
     "flex flex-col gap-1 first:font-medium last:text-lg last:text-gray-500";
   const navigate = useNavigate();
   const { checkout, isCheckingOut } = useCheckout();
+  const { deleteBooking, isDeletingBooking } = useDeleteBooking();
+
   return (
     <Table.Row>
-      <div className={cabinStyle}>{cabinName}</div>
+      <Modal>
+        <div className={cabinStyle}>{cabinName}</div>
 
-      <div className={stackStyle}>
-        <span className={spanStyle}>{guestName}</span>
-        <span className={spanStyle}>{email}</span>
-      </div>
+        <div className={stackStyle}>
+          <span className={spanStyle}>{guestName}</span>
+          <span className={spanStyle}>{email}</span>
+        </div>
 
-      <div className={stackStyle}>
-        <span className={spanStyle}>
-          {isToday(new Date(startDate))
-            ? "Today"
-            : formatDistanceFromNow(startDate)}{" "}
-          &rarr; {numNights} night stay
-        </span>
-        <span className={spanStyle}>
-          {format(new Date(startDate), "MMM dd yyyy")} &mdash;{" "}
-          {format(new Date(endDate), "MMM dd yyyy")}
-        </span>
-      </div>
-      <Tag type={status}>{status.replace("-", " ")}</Tag>
+        <div className={stackStyle}>
+          <span className={spanStyle}>
+            {isToday(new Date(startDate))
+              ? "Today"
+              : formatDistanceFromNow(startDate)}{" "}
+            &rarr; {numNights} night stay
+          </span>
+          <span className={spanStyle}>
+            {format(new Date(startDate), "MMM dd yyyy")} &mdash;{" "}
+            {format(new Date(endDate), "MMM dd yyyy")}
+          </span>
+        </div>
+        <Tag type={status}>{status.replace("-", " ")}</Tag>
 
-      <div className="text-lg">{formatCurrency(totalPrice)}</div>
-      <Menus.Toggle id={bookingId}></Menus.Toggle>
-      <Menus.List id={bookingId}>
-        <Menus.Button
-          icon={<HiEye></HiEye>}
-          onClick={() => navigate(`/bookings/${bookingId}`)}
-        >
-          See detail
-        </Menus.Button>
-        {status === "unconfirmed" && (
+        <div className="text-lg">{formatCurrency(totalPrice)}</div>
+        <Menus.Toggle id={bookingId}></Menus.Toggle>
+        <Menus.List id={bookingId}>
           <Menus.Button
-            icon={<HiArrowDownOnSquare></HiArrowDownOnSquare>}
-            onClick={() => navigate(`/checkin/${bookingId}`)}
+            icon={<HiEye></HiEye>}
+            onClick={() => navigate(`/bookings/${bookingId}`)}
           >
-            Check in
+            See detail
           </Menus.Button>
-        )}
+          {status === "unconfirmed" && (
+            <Menus.Button
+              icon={<HiArrowDownOnSquare></HiArrowDownOnSquare>}
+              onClick={() => navigate(`/checkin/${bookingId}`)}
+            >
+              Check in
+            </Menus.Button>
+          )}
 
-        {status === "checked-in" && (
-          <Menus.Button
-            icon={<HiArrowUpOnSquare></HiArrowUpOnSquare>}
-            onClick={() => {
-              checkout(bookingId);
+          {status === "checked-in" && (
+            <Menus.Button
+              icon={<HiArrowUpOnSquare></HiArrowUpOnSquare>}
+              onClick={() => {
+                checkout(bookingId);
+              }}
+              disabled={isCheckingOut}
+            >
+              Check out
+            </Menus.Button>
+          )}
+          <Modal.Open opens="deleteBooking">
+            <Menus.Button icon={<HiTrash></HiTrash>}>Delete</Menus.Button>
+          </Modal.Open>
+        </Menus.List>
+        <Modal.Window name="deleteBooking">
+          <ComfirmDelete
+            resourceName="booking"
+            disabled={isDeletingBooking}
+            onConfirm={() => {
+              deleteBooking(bookingId);
             }}
-            disabled={isCheckingOut}
-          >
-            Check out
-          </Menus.Button>
-        )}
-      </Menus.List>
+          ></ComfirmDelete>
+        </Modal.Window>
+      </Modal>
     </Table.Row>
   );
 }
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,9 @@
 function Login() {
-  return <div>Login</div>;
+  return (
+    <main className="min-h-[100vh] grid grid-cols-[48rem] content-center justify-center gap-6 bg-gray-50">
+      Login
+    </main>
+  );
 }
 
 export default Login;
